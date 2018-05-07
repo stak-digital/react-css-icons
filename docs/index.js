ef4a1bfac259b2313e345ab19feaadb045dc4366
Luke Boyle <luke.boyle.au@gmail.com>
2018-05-07 22:44:15 +1000

    chore: update demo

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Battery from '../lib/battery.jsx';
+import BatteryFull from '../lib/battery-solid.jsx';
+import Card from '../lib/card.jsx';
+import Camera from '../lib/camera.jsx';
+import Relieved from '../lib/relieved-smiley.jsx';
+
+function App() {
+	return (
+		<div className="container">
+			<Battery />
+			<BatteryFull />
+			<Card />
+			<Camera />
+			<Relieved />
+		</div>
+	);
+}
 
 render(
-	document.getElementById('root'),
-	Battery
+	<App />,
+	document.getElementById('root')
 );
